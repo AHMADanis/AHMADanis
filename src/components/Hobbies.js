48757ccb1ccfa45b66f7bb3d5baead4df53d6130
Anees Ahmad <inbox.a.ahmad@gmail.com>
2022-02-20 17:06:57 +0100

    done with hobies

--- src/components/Hobbies.js
+++ src/components/Hobbies.js
@@ -5,6 +5,7 @@ import { Text } from './common/Text'
 import { ImHeadphones } from 'react-icons/im';
 import { GrGamepad } from 'react-icons/gr';
 import { GiSecretBook, GiCommercialAirplane, GiPhotoCamera, GiVolleyballBall } from 'react-icons/gi';
+import Mycolors from './common/Mycolors';
 
 
 
@@ -28,27 +29,27 @@ export default function Hobbies() {
     return (
         <Row>
             <IconTextWrapper>
-                <Icon color="gray" size="24px"><ImHeadphones /></Icon>
-                <Text color="gray" size="24px">Music</Text>
+                <Icon color={Mycolors.evening} size="24px"><ImHeadphones /></Icon>
+                <Text color="navy" size="24px">Music</Text>
             </IconTextWrapper>
             <IconTextWrapper>
-                <Icon color="gray" size="24px"><GrGamepad /></Icon>
+                <Icon color={Mycolors.evening} size="24px"><GrGamepad /></Icon>
                 <Text color="gray" size="24px">Gaming</Text>
             </IconTextWrapper>
             <IconTextWrapper>
-                <Icon color="gray" size="24px"><GiSecretBook /></Icon>
+                <Icon color={Mycolors.evening} size="24px"><GiSecretBook /></Icon>
                 <Text color="gray" size="24px">Reading</Text>
             </IconTextWrapper>
             <IconTextWrapper>
-                <Icon color="gray" size="24px"><GiCommercialAirplane /></Icon>
+                <Icon color={Mycolors.evening} size="24px"><GiCommercialAirplane /></Icon>
                 <Text color="gray" size="24px">Traveling</Text>
             </IconTextWrapper>
             <IconTextWrapper>
-                <Icon color="gray" size="24px"><GiPhotoCamera /></Icon>
+                <Icon color={Mycolors.evening} size="24px"><GiPhotoCamera /></Icon>
                 <Text color="gray" size="24px">Photo Graphy</Text>
             </IconTextWrapper>
             <IconTextWrapper>
-                <Icon color="gray" size="24px"><GiVolleyballBall /></Icon>
+                <Icon color={Mycolors.evening} size="24px"><GiVolleyballBall /></Icon>
                 <Text color="gray" size="24px">Volley Ball</Text>
             </IconTextWrapper>
 
